6f7dbd3edc973f3004b0cd69eda8ceb2058293ad
Antonio Castiglione <antoniocastiglione10@gmail.com>
2019-06-28 11:38:33 -0400

    finished layout and styling of featured products section.

--- page-home.php
+++ page-home.php
@@ -31,27 +31,31 @@ $hero = get_field('hero_img');
 							</div>
 
 							<div class="products">
-								<div class="large-wrapper">
+								<div class="inner-products large-wrapper">
 
 									<?php if (have_rows('products')) : while (have_rows('products')) : the_row(); ?>
-											<h2 class="secondary-title"><?php $title = get_sub_field('title');
-																									if (!empty($title)) : echo $title;
-																									else : echo 'product name';
-																									endif; ?></h2>
-											<img src="<?php $img = get_sub_field('image');
-																if (!empty($img)) : echo $img[url];
-																else : echo get_template_directory_uri() . '/library/images/placeholder.png';
-																endif; ?>">
-											<p class="text"><?php $text = get_sub_field('text');
-																			if (!empty($text)) : echo $text;
-																			else : echo 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus sem quis mauris luctus, vitae sodales enim interdum. Praesent id odio in purus convallis accumsan ut id velit. In laoreet ac felis nec eleifend. Nulla tempor est nec malesuada feugiat. Proin lorem eros, fringilla congue magna ut, fermentum interdum quam. Suspendisse volutpat justo turpis, at accumsan diam pharetra ut.';
-																			endif; ?></p>
-											<a href="<?php $link = get_sub_field('cta_link');
-																if (!empty($link)) : echo $link;
-																else : echo '#';
-																endif; ?>" class="link view-more"><?php echo get_sub_field('cta_text'); ?></a>
+											<div class="product">
+												<div class="top">
+													<h2 class="secondary-title"><?php $title = get_sub_field('title');
+																											if (!empty($title)) : echo $title;
+																											else : echo 'product name';
+																											endif; ?></h2>
+													<img src="<?php $img = get_sub_field('image');
+																		if (!empty($img)) : echo $img[url];
+																		else : echo get_template_directory_uri() . '/library/images/placeholder.png';
+																		endif; ?>">
+												</div>
+												<p class="text"><?php $text = get_sub_field('text');
+																				if (!empty($text)) : echo $text;
+																				else : echo 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus sem quis mauris luctus, vitae sodales enim interdum. Praesent id odio in purus convallis accumsan ut id velit. In laoreet ac felis nec eleifend. Nulla tempor est nec malesuada feugiat. Proin lorem eros, fringilla congue magna ut, fermentum interdum quam. Suspendisse volutpat justo turpis, at accumsan diam pharetra ut.';
+																				endif; ?></p>
+												<a class="link" href="<?php $link = get_sub_field('cta_link');
+																							if (!empty($link)) : echo $link;
+																							else : echo '#';
+																							endif; ?>" class="link view-more"><?php echo get_sub_field('cta_text'); ?></a>
+											</div>
 										<?php endwhile;
-								endif; ?>
+									endif; ?>
 								</div>
 							</div>
 						</section>
@@ -59,7 +63,7 @@ $hero = get_field('hero_img');
 					</article>
 
 				<?php endwhile;
-		endif; ?>
+			endif; ?>
 
 		</main>
 
